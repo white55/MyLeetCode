2d5da60dc6d5948163f95c792e04d217650d90e3
zhangchao <2472461792@qq.com>
2017-11-24 18:02:23 +0800

    XOR 用异或的方法计算汉明距离

--- demo/src/main/java/com/train/HammingDistence.java
+++ demo/src/main/java/com/train/HammingDistence.java
@@ -3,9 +3,49 @@ package com.train;
 /**
  * Created by 张超 on 2017/11/24.
  */
+
 public class HammingDistence {
     public static void main(String[] args) {
-        String ss=Integer.toBinaryString(10);
-        System.out.println(ss);
+        HammingDistence hm=new HammingDistence();
+        int x=5;
+        int y=3;
+        hm.hd1(x,y);
+        hm.hm2(x,y);
+    }
+    //自己写的笨方法
+    void hd1(int x,int y){
+        int distense=0;
+        String xb=Integer.toBinaryString(x);
+        String yb=Integer.toBinaryString(y);
+        int diff=xb.length()-yb.length();
+        diff=Math.abs(diff);
+        int max=yb.length();
+        if(xb.length()>yb.length()){
+            max=xb.length();
+            for(int j=0;j<diff;j++){
+                yb="0"+yb;
+            }
+        }else {
+            for(int j=0;j<diff;j++){
+                xb="0"+xb;
+            }
+        }
+        for(int i=0;i<max;i++){
+            if(xb.charAt(i)!=yb.charAt(i)){
+                distense++;
+            }
+        }
+        System.out.println(distense);
+    }
+    //leetCode 上看到的位运算方法，很机智，学来的
+    //异或
+    void hm2(int x,int y){
+         int distance=0;
+         int xor=x^y;
+         while (xor>0){
+             ++distance;
+             xor=xor&(xor-1);
+         }
+        System.out.println(distance);
     }
 }
