57962b07f0d44a5a4c2c3920671044573addbfee
zhangchao <2472461792@qq.com>
2017-11-28 16:32:36 +0800

    test

---
+++ demo/src/main/java/com/train/ValidParentheses.java
@@ -0,0 +1,18 @@
+package com.train;
+
+/**
+ * Created by 张超 on 2017/11/27.
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if(s.length()==0){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
