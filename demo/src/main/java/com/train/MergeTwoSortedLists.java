0a15ff4a50e1f648ac03a87745ed6b552adf30f1
zhangchao <2472461792@qq.com>
2017-11-30 15:19:10 +0800

    递归查询

---
+++ demo/src/main/java/com/train/MergeTwoSortedLists.java
@@ -0,0 +1,19 @@
+package com.train;
+
+/**
+ * Created by 张超 on 2017/11/29.
+ */
+//QUESTION:
+//Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+    //合并两个排序列表并将其作为一个新列表返回，新的列表应该是由 前两个列表的节点拼接成的。
+public class MergeTwoSortedLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), cur = dummy;
+        return null;
+    }
+}
