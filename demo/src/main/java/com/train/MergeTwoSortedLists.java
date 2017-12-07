03ab3829cd36dd4aa18f8f4507b095d2acf740dd
zhangchao <2472461792@qq.com>
2017-12-07 14:24:53 +0800

    Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.

--- demo/src/main/java/com/train/MergeTwoSortedLists.java
+++ demo/src/main/java/com/train/MergeTwoSortedLists.java
@@ -12,8 +12,22 @@ public class MergeTwoSortedLists {
         ListNode next;
         ListNode(int x) { val = x; }
     }
+    //COPY
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1), cur = dummy;
-        return null;
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
     }
 }
