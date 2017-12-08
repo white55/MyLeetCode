b67cbde9ba2c63da03943a8a4387a9e9d1034047
zhangchao <2472461792@qq.com>
2017-12-08 18:37:20 +0800

    Given an array and a value, remove all instances of that value in-place and return the new length.
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    
    The order of elements can be changed. It doesn't matter what you leave beyond the new length.

---
+++ demo/src/main/java/com/train/RemoveElement.java
@@ -0,0 +1,40 @@
+package com.train;
+
+/**
+ * Created by 张超 on 2017/12/8.
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int length=nums.length;
+        for(int i=0;i<length;i++){
+            while (nums[i]==val&&i<length){
+                nums[i]=nums[--length];
+            }
+
+        }
+        for(int j=0;j<nums.length;j++){
+            System.out.println(nums[j]);
+        }
+        return nums.length;
+    }
+    //耗时最短的方法，我觉得最机智了！！！
+    public int removeElement2(int[] a, int val) {
+        int start = 0;
+        int len = a.length;
+        int begin = 0;
+        for (int i = 0; i < len; i++)
+            if (a[i] != val)
+                a[begin++] = a[i];
+        return begin;
+    }
+    public static void main(String[] args) {
+        RemoveElement removeElement=new RemoveElement();
+        int[] ss=new int[5];
+        ss[0]=1;
+        ss[1]=1;
+        ss[2]=2;
+        ss[3]=2;
+        ss[4]=1;
+        removeElement.removeElement(ss,1);
+    }
+}
