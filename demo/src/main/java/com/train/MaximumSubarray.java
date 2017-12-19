1bb02883b06a2fe8ae333c31c18f1cb190db25f1
zhangchao <2472461792@qq.com>
2017-12-19 17:34:03 +0800

    /Find the contiguous subarray within an array (containing at least one number) which has the largest sum.

--- demo/src/main/java/com/train/MaximumSubarray.java
+++ demo/src/main/java/com/train/MaximumSubarray.java
@@ -7,6 +7,19 @@ package com.train;
 //For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 //the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 public class MaximumSubarray {
+    //copy
+    public int maxSubArray2(int[] A) {
+        if(A==null || A.length==0)
+            return 0;
+        int global = A[0];
+        int local = A[0];
+        for(int i=1;i<A.length;i++)
+        {
+            local = Math.max(A[i],local+A[i]);
+            global = Math.max(local,global);
+        }
+        return global;
+    }
     public int maxSubArray(int[] nums) {
         return 0;
     }
